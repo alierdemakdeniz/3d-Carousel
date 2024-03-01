2ea351a71c8088ff2fee037c331cfb4ba06b8d9e
alierdemakdeniz <akdenizef@gmail.com>
2024-03-01 15:02:45 +0300

    update

--- app.js
+++ app.js
@@ -2,55 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const radios = document.getElementsByName("slider");
     const prev = document.querySelector(".arrow.prev");
     const next = document.querySelector(".arrow.next");
-    const dots = document.getElementsByClassName("dot");
-    const dot = Array.from(dots)
     const radio = Array.from(radios);
-    dot[0].setAttribute("style","background-color: red;");
-
-    
     let i = 0;
     function goToSlide(j) {
         i = j;
-        
         radio[i].checked = true;
-        
-        
     }
     function showNextSlide() {
         i++;
         if (i >= radio.length) {
-            dot[radio.length-1].setAttribute("style","background-color:grey;")
             i = 0;
         }
-        
-        if (i===0){
-            dot[i].setAttribute("style","background-color: red;");
-        }else{
-            
-            dot[i].setAttribute("style","background-color: red;");
-            dot[i-1].setAttribute("style","background-color: grey;");
-        }
-            
         goToSlide(i);
     }
 
     function showPrevSlide() {
         i--;
         if (i < 0) {
-            dot[0].setAttribute("style","background-color:grey;")
-            
             i = radio.length - 1;
-            
-        
         }
-        if (i===radio.length-1){
-            dot[i].setAttribute("style","background-color: red;");
-        }else{
-            
-            dot[i].setAttribute("style","background-color: red;");
-            dot[i+1].setAttribute("style","background-color: grey;");
-        }
-
         goToSlide(i);
     }
     next.addEventListener("click", showNextSlide);
@@ -65,10 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
             showPrevSlide();
         }
     });
-    
-
     // İlk slaydı göster
     goToSlide(0);
 });
-
-
